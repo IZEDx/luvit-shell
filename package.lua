1dd96a50faa07d45a614c43d2b5834a9b6316642
Niklas Kühtmann <nk@baltray.org>
2015-10-13 12:54:30 +0200

    Initial setup.

---
+++ package.lua
@@ -0,0 +1,12 @@
+return {
+  	name = "b42nk/shell",
+  	version = "0.1",
+  	files = {
+    	"*.lua",
+    	"libs/*.lua"
+  	},
+  	dependencies = {
+  		"luvit/luvit",
+  		"b42nk/websocket"
+	}
+}
